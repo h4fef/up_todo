e7d9764e00c7aec15212c873bae82581da1fd243
h4fef <afefhassani00@gmail.com>
2025-07-10 22:10:35 +0200

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 //flutter
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 //screen
 import 'package:up_todo/screens/preview_app/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
